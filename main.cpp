978a64df0e805e8ac493002c0c4177f84a1f5c6b
Earl Rex Arao-arao <earl2day@gmail.com>
2023-03-30 15:27:12 +0000

    added visual studio files and build folders

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
-/*
-Author: Earl Rex Arao-arao
-Email : earl2day@gmail.com
-*/
-
 #include <iostream>
 #include "Robot.h"
 #include "TableTop.h"
 #include "UserInput.h"
 
-int main()
+int main(int argc, char* argv[])
 {
- int xSize=-1, ySize=-1;
- for(;;)
- {
-  std::cout<< "Please enter table horizontal and vertical size Example: 10 10" << std::endl; 
-  std::cin >> xSize >> ySize;
-  if(xSize<0 || ySize<0) {xSize=-1; ySize=-1; continue;}
-  else break;
- }
+ //check if there is input file and
+  if (argc > 1)
+  {
+      //open the file
+      ///read the file per line
+      //first line should be table size
+      //second line should be initial location
+      //next if the moves like MOVE, REPORT, LEFT, RIGHT, EXIT
+  }
+  else
+  {
+      int xSize = -1, ySize = -1;
+      for (;;)
+      {
+          std::cout << "Please enter table horizontal and vertical size Example: 10 10" << std::endl;
+          std::cin >> xSize >> ySize;
+          if (xSize < 0 || ySize < 0) { xSize = -1; ySize = -1; continue; }
+          else break;
+      }
+  }
 
  TableTop table(xSize,ySize);
  Robot robot(&table);
